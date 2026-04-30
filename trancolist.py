1c7e52a1df05a7efeb4a4e5c78587338672ce250
Andrew Riachi <andrew.riachi@gmail.com>
2026-04-30 12:32:51 -0600

    add argument parser

---
+++ trancolist.py
@@ -0,0 +1,48 @@
+# Copyright 2026 The University of Utah
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+from tap import Tap, Positional
+from tranco import Tranco
+
+from credentials import load_credentials
+
+
+class ArgParser(Tap):
+    list_id: str
+    """
+    The list ID of an existing or pending list to get. Mutually exclusive with
+    --config.
+    """
+    config: Path = Path("default.json")
+    """
+    The path to a json file that configures a new Tranco list. The json file
+    should follow the schema at
+    https://tranco-list.eu/api_documentation#datatypes-configuration. Mutually
+    exclusive with --list-id.
+    """
+    cred_json: Path = Path("credentials.json")
+    """
+    The path to a json file with values for the keys "email" and "api_key".
+    Environment variables `TRANCO_EMAIL` and `TRANCO_API_KEY` take precedence,
+    so this option will be ignored if both are specified.
+    """
+    blocklists: set[Path] = {Path('adult-blocklist.txt')}
+    """
+    A list of files. Each file is a list of domains to exclude from the
+    final list, one on each line.
+    """
+    top_n: int = 200
+    """The number of domains to include"""
+    out_dir: Positional[Path]
+    """The directory to write the list and metadata into"""
+    force: bool = False
+    """Overwrite `out_dir` if it already exists"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
