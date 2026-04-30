40346529a2c5ba8369b21203a057bbd8edd4403e
Andrew Riachi <andrew.riachi@gmail.com>
2026-04-29 22:42:27 -0600

    add tranco module and credentials

---
+++ trancolist/tranco.py
@@ -0,0 +1,39 @@
+# Copyright 2026 The University of Utah
+# SPDX-License-Identifier: MIT
+
+from dataclasses import dataclass
+import json
+import os
+from pathlib import Path
+import sys
+
+
+@dataclass
+class Credentials:
+    email: str
+    api_key: str
+
+
+def load_credentials(cred_path: Path | None) -> Credentials:
+    """
+    Tries to load credentials from environment variables TRANCO_EMAIL and
+    TRANCO_API_KEY first, then from the json file at `cred_path`.
+    """
+    creds: dict[str, str | None] = { "email": None, "api_key": None }
+    if cred_path:
+        with open(cred_path) as f:
+            creds.update(json.load(f))
+    env_email = os.environ.get("TRANCO_EMAIL")
+    env_api_key = os.environ.get("TRANCO_API_KEY")
+    email, api_key = creds["email"], creds["api_key"]
+    if env_email:
+        email = env_email
+    if env_api_key:
+        api_key = env_api_key
+    if not email:
+        print("Missing email.")
+    if not api_key:
+        print("Missing API key.")
+    if not (email and api_key):
+        sys.exit(1)
+    return Credentials(email, api_key)
