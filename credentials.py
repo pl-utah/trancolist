871f47eaa4e946082c9bf2f3f48bf5066447fe87
Andrew Riachi <andrew.riachi@gmail.com>
2026-04-30 19:13:03 -0600

    improve credential handling
    don't use a default dict that gets updated

--- credentials.py
+++ credentials.py
@@ -19,17 +19,17 @@ def load_credentials(cred_path: Path | None) -> Credentials:
     Tries to load credentials from environment variables TRANCO_EMAIL and
     TRANCO_API_KEY first, then from the json file at `cred_path`.
     """
-    creds: dict[str, str | None] = { "email": None, "api_key": None }
+    json_email = None
+    json_api_key = None
     if cred_path and cred_path.is_file():
         with open(cred_path) as f:
-            creds.update(json.load(f))
+            creds = json.load(f)
+            json_email = creds.get("email")
+            json_api_key = creds.get("api_key")
     env_email = os.environ.get("TRANCO_EMAIL")
     env_api_key = os.environ.get("TRANCO_API_KEY")
-    email, api_key = creds["email"], creds["api_key"]
-    if env_email:
-        email = env_email
-    if env_api_key:
-        api_key = env_api_key
+    email = env_email if env_email is not None else json_email
+    api_key = env_api_key if env_api_key is not None else json_api_key
     if not email:
         print(f"Email was not found: not in {cred_path} and envvar TRANCO_EMAIL was not set.", file=sys.stderr)
     if not api_key:
