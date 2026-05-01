56f26dd77269321a5f160176cf9062ad955dadc7
Andrew Riachi <andrew.riachi@gmail.com>
2026-04-30 21:32:43 -0600

    clean up block.py api

--- block.py
+++ block.py
@@ -25,11 +25,12 @@ class Blocklist:
         return False
 
 
-def parse_args() -> Path:
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <tranco.csv>")
+def parse_args() -> tuple[list[Path], Path]:
+    """Returns (blocklists, tranco_list)"""
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} [blocklist1.txt ...] <trancolist.txt>")
         sys.exit(1)
-    return Path(sys.argv[1])
+    return (list(map(Path, sys.argv[1:-1])), Path(sys.argv[-1]))
 
 
 def parse_blocklists(lists: Iterable[Iterable[str]]) -> Blocklist:
@@ -37,22 +38,18 @@ def parse_blocklists(lists: Iterable[Iterable[str]]) -> Blocklist:
     return Blocklist(set(map(Domain, flattened))) # Create a set from all the lines
 
 
-def parse_adult_list() -> Blocklist:
-    with open('adult-blocklist.txt') as f:
-        return parse_blocklists(itertools.repeat(f, 1))
-
-
 def filter_domains(domains: Iterable[Domain], blocklist: Blocklist) -> Iterator[Domain]:
     return filter(lambda domain: not blocklist.blocks(domain), domains)
 
 
 def main():
-    tranco_path = parse_args()
+    block_paths, tranco_path = parse_args()
     tranco_list = map(tranco.parse_tranco_line, open(tranco_path))
-    adult_list = parse_adult_list()
+    blocklists = map(open, block_paths)
+    blocklist = parse_blocklists(blocklists)
     out_path = f"{tranco_path.stem}_filtered{tranco_path.suffix}"
     with open(out_path, 'w') as f:
-        (f.write(domain.inner) for domain in filter_domains(tranco_list, adult_list))
+        (f.write(domain.inner) for domain in filter_domains(tranco_list, blocklist))
 
 
 if __name__ == "__main__":
